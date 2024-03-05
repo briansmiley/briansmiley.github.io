4fa771cc442429b70f3015000f4e9e75de3e2b2b
Brian Smiley <34353764+briansmiley@users.noreply.github.com>
2024-03-05 15:03:34 -0500

    Create control_panel.js

---
+++ p5pages/binarytree/control_panel.js
@@ -0,0 +1,37 @@
+class ControlPanel {
+  constructor() {
+    this.container = this.createContainer();
+    this.controls = [];
+  }
+  createContainer() {
+    const container = createDiv();
+    container.id('control_panel');
+    container.parent(document.querySelector('main'));
+    return container;
+  }
+  //add a preExisting already-containerized control to the set
+  addControl(controlDiv) {
+    this.controls.push(panelDiv);
+    panelDiv.parent(this);
+  }
+  
+  //create a new textboxSlider() control 
+  addTextboxSlider(mn, mx, initial, step = 0, label = '', id = "textboxSlider") {
+    let slider = new textboxSlider(mn, mx, initial, step, this.container, label, id)
+    this.controls.push(slider);
+    return slider;
+  }
+  
+  addCheckbox(label = '',state = false,parent = this) {
+    let check = createCheckbox(label,state);
+    check.parent(parent.container);
+    this.controls.push(check);
+    return check;
+  }
+  
+  update() {
+    this.controls.forEach((setting) => {
+      if (typeof setting.update == 'function') setting.update();
+    });
+  }
+}
