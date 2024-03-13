cb9a1a0ecce8e733902a24a93bfd9c66c82b152d
Brian Smiley <briansmiley@Brians-MacBook-Pro.local>
2024-03-13 18:57:50 -0400

    first working draft

---
+++ game.js
@@ -0,0 +1,30 @@
+class Game {
+    constructor(paddleWidth = 7, paddleHeight = 2, ballSize = 2, ballFR = 5) {
+        this.paddle1 = new Paddle(1,paddleWidth,paddleHeight,width/2,2);
+        this.paddle2 = new Paddle(2,paddleWidth,paddleHeight,width/2, height-2);
+        this.ball = new Ball(ballSize,width/2,height/2,1,1);
+        this.score1 = 0;
+        this.score2 = 0;
+        this.ballFR = ballFR;
+    }
+
+    update() {
+        this.paddle1.update(value(slider1));
+        this.paddle2.update(value(slider2));
+        if (frameCount % this.ballFR == 0) this.ball.update([this.paddle1,this.paddle2]);
+    
+        this.paddle1.draw();
+        this.paddle2.draw()
+        this.ball.draw();
+        switch (this.ball.checkScore()) {
+            case 0:
+                break;
+            case 1:
+                score1++
+                break;
+            case 2:
+                score2++
+                break;
+        }
+    }
+}
