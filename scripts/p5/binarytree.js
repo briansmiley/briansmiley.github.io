19f1adc002f00046ce9682469b8006d7151dfd31
Brian Smiley <briantsmiley42@gmail.com>
2024-03-26 21:59:33 -0700

    creates p5 scripts folder and adds binarytree.js

---
+++ scripts/p5/binarytree.js
@@ -0,0 +1,29 @@
+var sclL = .75;
+var sclR = .75;
+function setup() {
+  createCanvas(900, 900);
+}
+
+function draw() {
+  
+  background(220);
+  strokeWeight (6);
+  translate(width/2,height);
+  let length = 180;
+  let angle = map(mouseY,0,height,0, PI);
+  branch(length,angle,6);
+}
+
+function branch(len,ang,wgt){
+  line(0,0,0,-len);
+  translate(0,-len);
+  strokeWeight(wgt)
+  if(len >= 5){
+    push();
+    rotate(ang);
+    branch(sclL*len,ang,max(.5,wgt*.7));
+    pop()
+    rotate(-ang);
+    branch(sclR*len,ang,max(.5,wgt*.7));
+  }
+}
